b50165cce1c74355bd6e879be56f2f228437a972
ErikaStott <erika.r.stott@gmail.com>
2018-09-10 14:48:05 -0700

    first commit

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,5 +1,56 @@
-function reducer(){
+const initialState = {
+    loanType: 'Home Purchase',
+    propertyType: 'Single Family Home',
+    city: '',
+    propToBeUsedOn: '',
+    found: "false",
+    realEstateAgent: "false",
+    cost: 0,
+    downPayment: 0,
+    credit: '',
+    history: '',
+    addressOne: '',
+    addressTwo: '',
+    addressThree: '',
+    firstName: '',
+    lastName: '',
+    email: ''
+}
 
+const UPDATE_LOAN_TYPE = {
+    type: "UPDATE_LOAN_TYPE"
+}
+
+const UPDATE_PROPERTY_TYPE = {
+    type: "UPDATE_PROPERTY_TYPE"
+}
+
+function reducer(state = initialState, action) {
+    switch(action.type) {
+    case "UPDATE_LOAN_TYPE" {
+        return (
+            initialState.loanType = "UPDATE_LOAN_TYPE"
+        );
+    }
+    case type: "UPDATE_PROPERTY_TYPE" {
+        retun (
+            initialState.propertyType = "UPDATE_PROPERTY_TYPE"
+        );
+    }
+}
+
+function updateLoanType(loanType){
+    return {
+    type: "UPDATE_LOAN_TYPE",
+    payload: loanType
+    }
+}
+
+function updatePropertyType(propertyType){
+    return {
+    type: "UPDATE_LOAN_TYPE",
+    payload: propertyType
+    }
 }
 
 export default reducer;
