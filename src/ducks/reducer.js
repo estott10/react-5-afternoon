4b21679896915aa98412574c65a2f11853daddf3
ErikaStott <erika.r.stott@gmail.com>
2018-09-10 16:48:21 -0700

    up to step 10

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -17,40 +17,79 @@ const initialState = {
     email: ''
 }
 
-const UPDATE_LOAN_TYPE = {
-    type: "UPDATE_LOAN_TYPE"
-}
+const UPDATE_LOAN_TYPE = "UPDATE_LOAN_TYPE";
 
-const UPDATE_PROPERTY_TYPE = {
-    type: "UPDATE_PROPERTY_TYPE"
-}
+
+const UPDATE_PROPERTY_TYPE = "UPDATE_PROPERTY_TYPE";
+
+
+const UPDATE_CITY = "UPDATE_CITY";
+
+
+const UPDATE_PROP = "UPDATE_PROP";
+
+const UPDATE_FOUND = 'UPDATE_FOUND';
 
 function reducer(state = initialState, action) {
-    switch(action.type) {
-    case "UPDATE_LOAN_TYPE" {
-        return (
-            initialState.loanType = "UPDATE_LOAN_TYPE"
-        );
-    }
-    case type: "UPDATE_PROPERTY_TYPE" {
-        retun (
-            initialState.propertyType = "UPDATE_PROPERTY_TYPE"
-        );
+    switch (action.type) {
+        case UPDATE_LOAN_TYPE:
+            return (
+                Object.assign({}, state, { loanType: action.payload })
+            );
+
+        case UPDATE_PROPERTY_TYPE:
+            return (
+                Object.assign({}, state, { propertyType: action.payload })
+            );
+
+        case UPDATE_CITY:
+            return (
+                Object.assign({}, state, { city: action.payload })
+            );
+
+        case UPDATE_PROP:
+            return (
+                Object.assign({}, state, { propToBeUsedOn: action.payload })
+            );
+            case UPDATE_FOUND:
+            return (
+                Object.assign( {}, state, { found: action.payload } )
+            );
+        default: return state;
     }
 }
 
-function updateLoanType(loanType){
+export function updateLoanType(loanType) {
     return {
-    type: "UPDATE_LOAN_TYPE",
-    payload: loanType
+        type: "UPDATE_LOAN_TYPE",
+        payload: loanType
     }
 }
 
-function updatePropertyType(propertyType){
+export function updatePropertyType(propertyType) {
     return {
-    type: "UPDATE_LOAN_TYPE",
-    payload: propertyType
+        type: "UPDATE_LOAN_TYPE",
+        payload: propertyType
     }
 }
+export function updateCity(city) {
+    return {
+        type: "UPDATE_CITY",
+        payload: city
+    }
+}
+export function updateProp(prop) {
+    return {
+        type: UPDATE_PROP,
+        payload: prop
+    }
+}
+
+export function updateFound( found ) {
+    return {
+      type: UPDATE_FOUND,
+      payload: found
+    }
+  }
 
 export default reducer;
