7813818f33854fd0825a97de3963e56baf2da8c9
ErikaStott <erika.r.stott@gmail.com>
2018-09-15 14:52:54 -0700

    completed

--- src/components/WizardSix/WizardSix.js
+++ src/components/WizardSix/WizardSix.js
@@ -1,21 +1,26 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { updateDownPayment } from '../../ducks/reducer';
+import { updateCost } from '../../ducks/reducer';
+import { connect } from 'react-redux';
 
 class WizardSix extends Component {
 
     render(){
+        const { updateDownPayment } = this.props;
+        const { updateCost } = this.props;
         return(
             <div className="parent-div">
                     <div className="vert-align">                    <p>What is the estimated purchase price?</p> <br />
                         
                         
-                        <input type="text" placeholder="amount" onChange={this.props.updateCost}/> <br />
+                        <input type="text" placeholder="amount" onChange={(e)=> updateCost(e.target.value)}/> <br />
 
 
                     <p>How much are you putting down as a down payment?</p> <br />
                         
                         
-                        <input type="text" placeholder="amount" onChange={this.props.updateDownPayment}/>                    
+                        <input type="text" placeholder="amount" onChange={(e) => updateDownPayment(e.target.value)}/>                    
                         
                     
                     <Link to="/wSeven"><button className="margin-btn"> Next </button></Link>
@@ -25,4 +30,12 @@ class WizardSix extends Component {
     }
 }
 
-export default WizardSix;
+function mapStateToProps(state) {
+    const { cost, downPayment } = state;
+    return {
+        cost,
+        downPayment
+    }
+}
+
+export default connect (mapStateToProps, { updateCost, updateDownPayment })(WizardSix);
--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './store'
+import store from './store';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <HashRouter>
-            <App />
-        </HashRouter>
-    </Provider>
-    , document.getElementById('root'));
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>    
+, document.getElementById('root'));
 registerServiceWorker();
 
